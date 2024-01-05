735856d64b4c5960a62b48dac8e073cbcbda26c8
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-01-05 19:58:44 +0900

    쇼핑 리스트 화면 구현

---
+++ TableViewController/ShoppingTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingTableViewCell.swift
+//  TableViewController
+//
+//  Created by 조유진 on 1/5/24.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+    @IBOutlet weak var cellBackgroundView: UIView!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var shopTodoLabel: UILabel!
+    @IBOutlet weak var starButton: UIButton!
+}
---
+++ TableViewController/ShoppingTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ShoppingTableViewController.swift
+//  TableViewController
+//
+//  Created by 조유진 on 1/5/24.
+//
+
+import UIKit
+
+class ShoppingTableViewController: UITableViewController {
+
+    @IBOutlet weak var textFieldBackgroundView: UIView!
+    @IBOutlet weak var shopTodoTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var shoppingTodoList = ["그립톡 구매하기", "사이다 구매", "아이패드 케이스 최저가 알아보기", "가디건 구매하기"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        designBackgroundView(textFieldBackgroundView)
+        designTextField(shopTodoTextField, placeHolder: "무엇을 구매하실 건가요?")
+        designButton(addButton, title: "추가")
+    }
+    
+    // TODO: - 예외 처리
+    @IBAction func shopTodoTextFieldDidReturn(_ sender: UITextField) {
+        shoppingTodoList.append(shopTodoTextField.text!)
+        tableView.reloadData()
+        shopTodoTextField.text = ""
+    }
+    
+    // TODO: - 예외 처리
+    @IBAction func addButtonClicked(_ sender: UIButton) {
+        shoppingTodoList.append(shopTodoTextField.text!)
+        tableView.reloadData()
+        shopTodoTextField.text = ""
+    }
+    
+    func designBackgroundView(_ view: UIView) {
+        view.backgroundColor = .systemGray6
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+    }
+    
+    func designTextField(_ textField: UITextField, placeHolder: String) {
+        textField.backgroundColor = .systemGray6
+        textField.placeholder = placeHolder
+        textField.layer.borderColor = UIColor.clear.cgColor
+        textField.layer.borderWidth = 0
+    }
+    
+    func designButton(_ button: UIButton, title: String) {
+        button.backgroundColor = .systemGray5
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingTodoList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath) as! ShoppingTableViewCell
+        
+        cell.cellBackgroundView?.backgroundColor = .systemGray6
+        cell.cellBackgroundView?.clipsToBounds = true
+        cell.cellBackgroundView?.layer.cornerRadius = 8
+        cell.checkButton.setImage(UIImage(named: "checkBox"), for: .normal)
+        cell.checkButton.tintColor = .black
+        cell.shopTodoLabel.text = shoppingTodoList[indexPath.row]
+        cell.shopTodoLabel.font = .systemFont(ofSize: 14)
+        cell.starButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        cell.starButton.tintColor = .black
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
