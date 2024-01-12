d0876a989052058c5d9e3f516b41c04cee2e010f
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-01-12 13:44:29 +0900

    grade 값에 따라 star 아이콘 출력 구현

--- TableViewController/Cell/TravelTableViewCell.swift
+++ TableViewController/Cell/TravelTableViewCell.swift
@@ -14,11 +14,11 @@ class TravelTableViewCell: UITableViewCell, TableViewCellProtocol {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var gradeLabel: UILabel!
-    @IBOutlet weak var saveLabel: UILabel!
     @IBOutlet weak var savedValueLabel: UILabel!
     @IBOutlet weak var travelImageView: UIImageView!
     
     @IBOutlet weak var heartButton: UIButton!
+    @IBOutlet var starImageViews: [UIImageView]!
     
     
     override func awakeFromNib() {
@@ -27,11 +27,16 @@ class TravelTableViewCell: UITableViewCell, TableViewCellProtocol {
         titleLabel.design(font: .boldSystemFont(ofSize: 14), numberOfLines: 2)
         descriptionLabel.design(textColor: .darkGray, font: .systemFont(ofSize: 12), numberOfLines: 2)
         gradeLabel.design(textColor: .gray, font: .systemFont(ofSize: 10))
-        saveLabel.design(text: "• 저장", textColor: .gray, font: .systemFont(ofSize: 10))
         savedValueLabel.design(textColor: .gray, font: .systemFont(ofSize: 10))
         travelImageView.contentMode = .scaleAspectFill
         heartButton.tintColor = .black
-        saveLabel.isHidden = false
+        
+        for star in starImageViews {
+            star.contentMode = .scaleAspectFit
+            star.image = UIImage(systemName: "star")
+            star.tintColor = .yellow
+            star.isHidden = true
+        }
     }
     
     override func draw(_ rect: CGRect) {
@@ -53,15 +58,37 @@ class TravelTableViewCell: UITableViewCell, TableViewCellProtocol {
         }
         if let grade = travel.grade {
             gradeLabel.text = "\(grade)"
-        }
-        if let save = travel.save {
-            saveLabel.isHidden = false
-            savedValueLabel.text = "\(save.formatterStyle(.decimal)!)"
-        } else {
-            saveLabel.isHidden = true
+            fillStar(grade: grade)
         }
         
+        
+        
+        if let save = travel.save {
+            savedValueLabel.text = "• 저장 \(save.formatterStyle(.decimal)!)"
+        }
         
         travelImageView.kf.setImage(with: travel.travel_img_url, placeholder: UIImage(named: "sunrise"))
+    }
+    
+    func fillStar(grade: Double) {
+        let fillStarCount: Int = Int(grade / 1)
+        let halfStarValue = grade - Double(fillStarCount)
+        
+        let halfImage: UIImage?
+        if halfStarValue < 0.5 {
+            halfImage = nil
+        } else {
+            halfImage = UIImage(systemName: "star.leadinghalf.filled")
+        }
+        
+        for index in 0...fillStarCount - 1 {
+            starImageViews[index].isHidden = false
+            starImageViews[index].image = UIImage(systemName: "star.fill")
+        }
+        
+        if let halfImage {
+            starImageViews[fillStarCount].isHidden = false
+            starImageViews[fillStarCount].image = halfImage
+        }
     }
 }
